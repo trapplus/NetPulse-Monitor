52a4ffa6ed832c6b487abab9b8c0f60ff0d26ebe
Eviltrapgod <blazer19092008@gmail.com>
2026-03-24 10:58:36 +0300

    Implement ARP parsing in NetworkDeviceProvider

--- include/Data/NetworkDeviceProvider.hpp
+++ include/Data/NetworkDeviceProvider.hpp
@@ -1,5 +1,9 @@
 #pragma once
 #include "Data/IDataProvider.hpp"
+#include <atomic>
+#include <mutex>
+#include <string>
+#include <vector>
 
 class NetworkDeviceProvider : public IDataProvider
 {
@@ -9,4 +13,16 @@ public:
 
     void fetch() override;
     void stop() override;
+
+private:
+    struct DeviceInfo {
+        std::string ip;
+        std::string mac;
+        std::string iface;
+        std::string status;
+    };
+
+    mutable std::mutex      m_mutex;
+    std::vector<DeviceInfo> m_data;
+    std::atomic<bool>       m_stopped { false };
 };
